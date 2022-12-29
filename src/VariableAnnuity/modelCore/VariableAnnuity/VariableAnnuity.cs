07b7df1e7189d68fc71373afaf53f5c7cc32b220
Yoshi Taniguchi <ytanigucmc@gmailc.om>
2022-12-29 09:33:14 -0500

    Added VariableAnnuity class

---
+++ src/VariableAnnuity/modelCore/VariableAnnuity/VariableAnnuity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableAnnuity
+{
+    public class VariableAnnuity
+    {
+        public DateTime contractDate { get; protected set; }
+        public BasePolicyHolder contractOwner { get; protected set; }
+
+    }
+}
